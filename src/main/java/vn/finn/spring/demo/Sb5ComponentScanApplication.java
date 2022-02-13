434649538ff05caed5a4fabefad44d57bb396c0a
finn-01 <nguyensam576@gmail.com>
2022-02-13 22:01:14 +0700

    ComponentScan, ...

--- src/main/java/vn/finn/spring/demo/Sb5ComponentScanApplication.java
+++ src/main/java/vn/finn/spring/demo/Sb5ComponentScanApplication.java
@@ -3,7 +3,19 @@ package vn.finn.spring.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 import vn.finn.spring.demo.other.OtherGirl;
+import vn.finn.spring.demo.other2.OtherGirl2;
+
+//@ComponentScan
+//@ComponentScan("vn.finn.spring.demo.other")
+
+//@ComponentScan Multiple
+@ComponentScan({"vn.finn.spring.demo.other", "vn.finn.spring.demo.other2"})
+
+//scanBasePackages
+//@SpringBootApplication(scanBasePackages  = "vn.finn.spring.demo.other")
+//
 
 @SpringBootApplication
 public class Sb5ComponentScanApplication {
@@ -22,6 +34,15 @@ public class Sb5ComponentScanApplication {
             OtherGirl otherGirl = context.getBean(OtherGirl.class);
             if(otherGirl != null){
                 System.out.println("Bean: " + otherGirl.toString());
+            }
+        }catch (Exception e){
+            System.out.println("Bean Girl not found");
+        }
+
+        try{
+            OtherGirl2 otherGirl2 = context.getBean(OtherGirl2.class);
+            if(otherGirl2 != null){
+                System.out.println("Bean: " + otherGirl2.toString());
             }
         }catch (Exception e){
             System.out.println("Bean Girl not found");
---
+++ src/main/java/vn/finn/spring/demo/other2/OtherGirl2.java
@@ -0,0 +1,11 @@
+package vn.finn.spring.demo.other2;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class OtherGirl2 {
+    @Override
+    public String toString() {
+        return "OtherGirl2.java";
+    }
+}
