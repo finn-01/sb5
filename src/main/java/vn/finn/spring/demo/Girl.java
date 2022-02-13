8497e5794542b5bb3d4556309bcf4f547ecee403
finn-01 <nguyensam576@gmail.com>
2022-02-13 21:47:05 +0700

    chua su dung componet scan

---
+++ src/main/java/vn/finn/spring/demo/Girl.java
@@ -0,0 +1,11 @@
+package vn.finn.spring.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Girl {
+    @Override
+    public String toString() {
+        return "Girl.java";
+    }
+}
--- src/main/java/vn/finn/spring/demo/Sb5ComponentScanApplication.java
+++ src/main/java/vn/finn/spring/demo/Sb5ComponentScanApplication.java
@@ -2,12 +2,30 @@ package vn.finn.spring.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import vn.finn.spring.demo.other.OtherGirl;
 
 @SpringBootApplication
 public class Sb5ComponentScanApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Sb5ComponentScanApplication.class, args);
+        ApplicationContext context = SpringApplication.run(Sb5ComponentScanApplication.class, args);
+
+        try{
+            Girl girl = context.getBean(Girl.class);
+            System.out.println("Bean: " + girl.toString());
+        }catch (Exception e){
+            System.out.println("No bean Girl");
+        }
+
+        try{
+            OtherGirl otherGirl = context.getBean(OtherGirl.class);
+            if(otherGirl != null){
+                System.out.println("Bean: " + otherGirl.toString());
+            }
+        }catch (Exception e){
+            System.out.println("Bean Girl not found");
+        }
     }
 
 }
