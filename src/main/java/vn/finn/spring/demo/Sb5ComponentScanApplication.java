522a5c7686d84b9a3823ed9ca0e9a6e62d19177f
finn-01 <nguyensam576@gmail.com>
2022-02-13 21:33:51 +0700

    set up

---
+++ src/main/java/vn/finn/spring/demo/Sb5ComponentScanApplication.java
@@ -0,0 +1,13 @@
+package vn.finn.spring.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb5ComponentScanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb5ComponentScanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vn/finn/spring/demo/Sb5ComponentScanApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.finn.spring.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb5ComponentScanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
